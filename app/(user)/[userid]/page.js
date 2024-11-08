b5c08aef1cfc3bceb690f6a7b29a684bd55c4d9f
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-11-09 00:22:17 +0200

    frt

--- app/(user)/[userid]/page.js
+++ app/(user)/[userid]/page.js
@@ -1,5 +1,5 @@
 import GetRouteData from "@/lib/GetRouteData";
-export default async function Page() {
+export default async function Page({params}) {
   const { userid } = await params;
   const data = await GetRouteData(userid);
   return <h1>{data.bio}</h1>;
