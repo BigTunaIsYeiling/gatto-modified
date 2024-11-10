a7c40ca6db6ad49968333311f4b60cecabe5c94d
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-11-10 11:49:36 +0200

    readNot

--- components/Notifications/NotificationsPage.js
+++ components/Notifications/NotificationsPage.js
@@ -1,7 +1,15 @@
 "use client";
 import NotificationList from "@/components/Notifications/NotificationList";
+import SetReadNot from "@/lib/SetReadNot";
 import { Box, Typography, Container } from "@mui/material";
+import { useEffect } from "react";
 const NotificationsPage = ({ data }) => {
+  useEffect(() => {
+    const updateReads = async () => {
+      await SetReadNot();
+    };
+    updateReads();
+  }, []);
   return (
     <Container>
       <Box sx={{ mt: 4 }}>
