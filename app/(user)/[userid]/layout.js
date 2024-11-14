352d79d3c91626b632d1d39e1296bb493a8d3347
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-11-14 20:13:27 +0200

    awa

--- app/(user)/[userid]/layout.js
+++ app/(user)/[userid]/layout.js
@@ -23,7 +23,7 @@ export async function generateMetadata({ params }) {
         ],
       },
       twitter: {
-        card: "summary_large_image",
+        card: "summary",
         title: `${data?.username || "User"}'s profile`,
         description: `Learn more about ${data?.username || "this user"}.`,
         images: [data?.avatar],
